3b954887856d80b607d884a9b0b4ba3189f93e70
Vrezh Fedora <vrezhraiden@gmail.com>
2023-05-26 18:13:11 +0200

    Model training

--- main.py
+++ main.py
@@ -67,35 +67,30 @@ def train_machine_learning_model():
     # Load and preprocess my MIDI data
     file_directory = "./adl-piano-midi/Children"
     midi_files = load_midi_files(file_directory)
-
     print("Loaded {} MIDI files".format(len(midi_files)))
 
     preprocessed_midi_data = [preprocess_midi_data(parse_midi_file(file)) for file in midi_files]
-
     print("Preprocessed {} MIDI files".format(len(preprocessed_midi_data)))
 
     # Split the preprocessed MIDI data into training and validation sets
     train_data, validation_data = split_train_validation_data(preprocessed_midi_data)
-
     print("Split {} MIDI files into {} training files and {} validation files".format(len(preprocessed_midi_data), len(train_data), len(validation_data)))
 
     # Further preprocess the MIDI data to create input sequences and corresponding labels for training
     train_sequences, train_labels = preprocess_data_for_training(train_data)
-
     print("Created {} training sequences and {} training labels".format(len(train_sequences), len(train_labels)))
 
     validation_sequences, validation_labels = preprocess_data_for_training(validation_data)
-    
     print("Created {} validation sequences and {} validation labels".format(len(validation_sequences), len(validation_labels)))
 
     # Determine input_shape and num_classes based on preprocessed data
-    input_shape = train_sequences.shape[1:]  # Use the shape of the sequences for input_shape
-    num_classes = len(np.unique(train_labels))  # Determine the number of unique classes in train_labels
-
+    input_shape = train_sequences.shape[1:] # Use the shape of the sequences for input_shape
+    num_classes = np.max(train_labels) + 1 # Determine the number of unique classes in train_labels
+    
     # Train the LSTM model
     model, history = train_lstm_model(train_sequences, train_labels, validation_sequences, validation_labels, input_shape, num_classes)
-
     return model, history
+
 
 
 def filter_midi_data(midi_data, model):
